ddbedec017a49ac9e0bdea1dcd5aed9df3e8d9e3
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-05-12 19:16:38 +0500

    Update purchase_request.py

--- purchase_request/models/purchase_request.py
+++ purchase_request/models/purchase_request.py
@@ -276,10 +276,10 @@ class PurchaseRequest(models.Model):
         return self.write({"state": "to_approve"})
 
     def button_approved(self):
-        if self.x_studio_product_type == 'IT Equipment':
-            return self.write({"state": "mis_approval"})
-        else:
-            return self.write({"state": "pre_approve"})
+#         if self.x_studio_product_type == 'IT Equipment':
+#             return self.write({"state": "mis_approval"})
+#         else:
+         return self.write({"state": "pre_approve"})
 
     def button_rejected(self):
         self.mapped("line_ids").do_cancel()
