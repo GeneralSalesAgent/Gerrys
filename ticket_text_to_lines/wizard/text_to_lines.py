2e1f438cff25e910806716b411c681937729c9ee
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-05-30 13:27:45 +0500

    Update text_to_lines.py

--- ticket_text_to_lines/wizard/text_to_lines.py
+++ ticket_text_to_lines/wizard/text_to_lines.py
@@ -60,14 +60,14 @@ class TicketText(models.TransientModel):
                 
                 base_fare = val[5].replace(',','.')
                 ps = val[14].replace(',','.')
-                fare = float(base_fare) + float(ps)
+#                 fare = float(base_fare) + float(ps)
                 fuel_charges = val[7].replace(',','.')
                 total_tax = val[6].replace(',','.')
                 #create pax lines
                 self.env['x_pax_sales_line'].create({
                 'x_studio_pax_sales_id': pax_sales.id,
                 'x_studio_passenger': partner_id.id,
-                'x_studio_base_fare': fare,
+                'x_studio_base_fare': base_fare,
                 'x_studio_ticket_': val[3],
                 'x_studio_fuel_charges': fuel_charges,
                 'x_studio_total_tax': total_tax,
