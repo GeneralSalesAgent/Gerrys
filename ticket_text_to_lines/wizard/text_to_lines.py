be837abaa89b38c614369d2a1e7478cf95e3f35e
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-06-01 01:34:22 +0500

    Update text_to_lines.py

--- ticket_text_to_lines/wizard/text_to_lines.py
+++ ticket_text_to_lines/wizard/text_to_lines.py
@@ -60,7 +60,7 @@ class TicketText(models.TransientModel):
                 
                 if val[0] != 'Date':
                     val[0] = date(1900, 1, 1) + timedelta(int(val[0])-2)
-                    pax_sales.date = val[0]
+                    pax_sales.x_studio_date = val[0]
                 
                 base_fare = val[5].replace(',','.')
                 ps = val[14].replace(',','.')
