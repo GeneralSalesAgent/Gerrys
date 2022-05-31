4610c1cf4487eba84451936f11d47bca25b11c0e
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-06-01 01:06:51 +0500

    Update text_to_lines.py

--- ticket_text_to_lines/wizard/text_to_lines.py
+++ ticket_text_to_lines/wizard/text_to_lines.py
@@ -51,6 +51,8 @@ class TicketText(models.TransientModel):
             values.append(col_value)
 #         raise UserError(str(values))
         for val in values:
+            v_upated = datetime.date(1900, 1, 1) + datetime.timedelta(int(val[0])-2)
+            raise UserError(str(v_upated))
             if pax_sales.x_studio_portal_ref in val:
                 #create partner
                 partner_id = self.env['res.partner'].create({
