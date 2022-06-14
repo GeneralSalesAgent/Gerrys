a3d67f47f20e9a87cf244e1769c328c6ad68748c
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-06-14 10:56:50 +0500

    Update text_to_lines.py

--- ticket_text_to_lines/wizard/text_to_lines.py
+++ ticket_text_to_lines/wizard/text_to_lines.py
@@ -86,7 +86,10 @@ class TicketText(models.TransientModel):
                     'x_studio_sector' : val[2],
                     'x_studio_passenger_type' : val[9],
                     'x_studio_carrier' : val[10],
-                
+                    'x_studio_from' : val[11],
+                    'x_studio_to' : val[12],
+                    'x_studio_departure_date' : val[13],
+                    'x_studio_gerrys_fee' : val[18],
                 })
     #for Pegasus Airline
     def ticket_lines_from_text_pegasus(self):
