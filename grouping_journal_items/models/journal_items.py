0603217ccbcb7f353ec04952864958b8fc8559b5
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-06-25 18:13:54 +0500

    Update journal_items.py

--- grouping_journal_items/models/journal_items.py
+++ grouping_journal_items/models/journal_items.py
@@ -91,7 +91,8 @@ class AccountMove(models.Model):
                     if data != total_ids[0] and data[2]['planned_amount'] != 0:
                         for key, value in account_amount.items():
                             if key in data[2]['account'] and value != data[2]['planned_amount']:
-                                data[2]['planned_amount'] = data[2]['planned_amount']/column_counts
+                                data[2]['planned_amount'] = data[2]['planned_amount']
+#                                 data[2]['planned_amount'] = data[2]['planned_amount']/column_counts
                                 break
             except:
                 pass
