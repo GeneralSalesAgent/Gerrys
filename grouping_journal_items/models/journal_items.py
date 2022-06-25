9965237fb1e1803dbb64fc43b16d4275a856a3f7
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-06-25 18:36:41 +0500

    Update journal_items.py

--- grouping_journal_items/models/journal_items.py
+++ grouping_journal_items/models/journal_items.py
@@ -89,7 +89,6 @@ class AccountMove(models.Model):
             #Asir Custom Code
             try:
                 for data in total_ids:
-                    raise UserError(str(data))
                     if data != total_ids[0] and data[2]['planned_amount'] != 0:
                         for key, value in account_amount.items():
                             if key in data[2]['account'] and value != data[2]['planned_amount']:
