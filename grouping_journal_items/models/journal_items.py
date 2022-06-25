be70ff50c61f565284a87d788bcc44b973ead6d8
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-06-25 18:19:48 +0500

    Update journal_items.py

--- grouping_journal_items/models/journal_items.py
+++ grouping_journal_items/models/journal_items.py
@@ -84,7 +84,7 @@ class AccountMove(models.Model):
                     d = {**d, **{col: totals.loc[idx, col] for col in totals}}
                     column_counts+=1
                     total_ids.append((0, 0, d))
-#             #raise UserError(str(total_ids))
+            raise UserError(str(total_ids))
             #Asir Custom Code
             try:
                 for data in total_ids:
