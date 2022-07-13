9f1010c1794005b66369cf76b5b4780957e24ab3
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-07-13 12:47:05 +0500

    New App Added

---
+++ attendance_workentries/wizard/attendance_workentries.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from calendar import month_abbr
+from datetime import datetime,date,timedelta
+import pty
+from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from odoo import models, fields, api
+from odoo.exceptions import UserError
+import base64
+import re
+from xlrd import open_workbook
+
+class AttendanceWorkentries(models.TransientModel):
+    _name = 'attendance.workentries'
+
+    # file_to_upload = fields.Binary(string="Attendance File")
+    # file_name = fields.Char(string="Filename")
+            
+    def attendance_workentries_from_api(self):
+        raise UserError("Haris")
