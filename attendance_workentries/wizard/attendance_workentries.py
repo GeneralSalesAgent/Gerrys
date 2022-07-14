e82f6a98e4d27027af558ba6a791f191a19371c3
GeneralSalesAgent <101710331+GeneralSalesAgent@users.noreply.github.com>
2022-07-14 13:30:47 +0500

    Update attendance_workentries.py

--- attendance_workentries/wizard/attendance_workentries.py
+++ attendance_workentries/wizard/attendance_workentries.py
@@ -145,11 +145,12 @@ class AttendanceWorkentries(models.TransientModel):
                     datee = datetime.fromisoformat(checkDate)
                     datee = datee + timedelta(days = 1)
                     datee = str(datee)
+                    checkTime2 = datee.split(' ')[0]
                     id = models_odoo.execute_kw(db_odoo, uid_odoo, password_odoo, 'hr.work.entry', 'create', [{
                     'name': "Attendance: "+record['employee_id'][1],
                     'employee_id': record['employee_id'][0],
-                    'date_start': datee+" "+"04:00:00",
-                    'date_stop':  datee+" "+"12:00:00",
+                    'date_start': checkTime2+" "+"04:00:00",
+                    'date_stop':  checkTime2+" "+"12:00:00",
                     # 'duration': '08.00',
                     'work_entry_type_id': 5,
                     'state': 'draft'
